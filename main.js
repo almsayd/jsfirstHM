d5831fe332dce3cff29a1fcd56dfec339a619ff8
Akmal <qwertyui@we.uz>
2020-12-25 15:07:15 +0500

    готовый файл

---
+++ main.js
@@ -0,0 +1,29 @@
+
+var user_name = prompt('Введиите ваше имя: ')
+console.info(user_name)
+
+var user_age =  +prompt('Введите ваш возраст: ')
+alert('Привет ' + user_name + ' Тебе ' + user_age + ' лет' + ' Я ванга)')
+console.info(user_age)
+
+var addition = +prompt('Решите пример:  6 + 3 = ?')
+var add = 6 + 3
+console.info('6 + 3 = ' + add + "  ( ваш ответ " + addition + ')')
+
+var subtraction = +prompt('Решите пример:  100 - 5 = ?')
+var sub = 100 - 5
+console.info('100 - 5 = ' + sub + "  ( ваш ответ " + subtraction + ')')
+
+var multiplication = +prompt("Решите пример:  2 * 9 = ?")
+var mult = 2 * 9
+console.info('2 * 9 = ' + mult + '  ( ваш ответ ' + multiplication + ')')
+
+var division = +prompt('Решите пример:  8 / 2 = ?')
+var div = 8 / 2
+console.info('8 / 2 = ' + div + '  ( ваш ответ ' + division + ')')
+
+var dwr = +prompt("Решите пример: 4 % 3 = ? ")
+var Dwr = 4 % 3
+console.info(' 4 % 3 = ' + Dwr + '  ( ваш ответ ' + dwr + ')')
+
+alert('Зайдите в консоль разработчика! И сверьте ответы')
